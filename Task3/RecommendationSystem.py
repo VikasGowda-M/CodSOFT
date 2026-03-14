34746b0c8b58f155cc8306448f5eb9e295855293
VikasGowda M <vikasmanjunath2005@gmail.com>
2026-03-14 12:40:41 +0530

    completed Task3 Rcommendation System

--- Task3/RecommendationSystem.py
+++ Task3/RecommendationSystem.py
@@ -1,5 +1,5 @@
 items = {
-    "movies ":{
+    "movies":{
         "avengers": "action",
         "titanic": "romance",
         "inception": "sci-fi",
@@ -16,5 +16,23 @@ items = {
         "nike shoes": "footwear",
         "samsung tv": "electronics",
         "adidas hoodie": "clothing"
+    }
 }
-}
+print("Categories: movies, books, products")
+category = input("Enter a category: ").strip().lower()
+if category in items:
+    print("Available types:")
+    types = set(items[category].values())
+    for t in types:
+        print("- " + t)
+    preferences = input("Enter your prefered type: ").strip().lower()
+    print("Recommended items:")
+    found = False
+    for item, item_type in items[category].items():
+        if item_type.lower() == preferences.lower():
+                print("- " + item)
+                found = True
+        if not found:
+            print("No items found for the selected type.")
+else:
+    print("Invalid category. Please choose from movies, books, or products.")
