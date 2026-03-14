1e19526602fd93858930e5ee94e6c12f9f82ccac
VikasGowda M <vikasmanjunath2005@gmail.com>
2026-03-14 13:12:25 +0530

    Improved Recommendation system with while loop and retry option

--- Task3/RecommendationSystem.py
+++ Task3/RecommendationSystem.py
@@ -18,21 +18,27 @@ items = {
         "adidas hoodie": "clothing"
     }
 }
-print("Categories: movies, books, products")
-category = input("Enter a category: ").strip().lower()
-if category in items:
+while True:
+    print("\nCategories: movies, books, products")
+    category = input("Enter a category: ").strip().lower()
+    if category not in items:
+        print("Invalid category. Please choose from movies, books, or products.")
+        continue
     print("Available types:")
     types = set(items[category].values())
     for t in types:
-        print("- " + t)
+         print("- " + t)
     preferences = input("Enter your prefered type: ").strip().lower()
     print("Recommended items:")
     found = False
     for item, item_type in items[category].items():
-        if item_type.lower() == preferences.lower():
-                print("- " + item)
-                found = True
-        if not found:
-            print("No items found for the selected type.")
-else:
-    print("Invalid category. Please choose from movies, books, or products.")
+         if item_type.lower() == preferences.lower():
+            print("- " + item)
+            found = True
+         if not found:
+                print("No items found for the selected type.")
+    again=input("Do you want to try again? (yes/no): ").strip().lower()
+    if again != "yes":
+        print("Thank you for using the recommendation system. Goodbye!")
+        break
+       
