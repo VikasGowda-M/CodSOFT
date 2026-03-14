e842f6d27129ceeb2f7bc399763cd569f7898a37
VikasGowda M <vikasmanjunath2005@gmail.com>
2026-03-14 12:18:39 +0530

    added dictionary to task 3

---
+++ Task3/RecommendationSystem.py
@@ -0,0 +1,20 @@
+items = {
+    "movies ":{
+        "avengers": "action",
+        "titanic": "romance",
+        "inception": "sci-fi",
+        "the godfather": "crime"
+    },
+    "books": {
+        "harry potter": "fantasy",
+        "the great gatsby": "classic",
+        "to kill a mockingbird": "historical fiction",
+        "1984": "dystopian"
+    },
+    "products": {
+        "iphone": "electronics",
+        "nike shoes": "footwear",
+        "samsung tv": "electronics",
+        "adidas hoodie": "clothing"
+}
+}
